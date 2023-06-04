8cfbf4b43f60f118fd968096759cf03b52b9598a
Henri Remonen <henri@remonen.fi>
2023-06-04 11:08:32 +0300

    Update user validation

--- src/server/Entities/user_entity.py
+++ src/server/Entities/user_entity.py
@@ -6,5 +6,5 @@ class NewUserSchema(Schema):
     username = fields.Str(required=True, validate=validate.Length(min=3))
     password = fields.Str(required=True, validate=validate.Length(min=8))
     passwordConfirm = fields.Str(required=True, validate=validate.Equal(password))
-    
+    role = fields.Str(validate=validate.OneOf(["user", "vendor", "admin"]))
 
