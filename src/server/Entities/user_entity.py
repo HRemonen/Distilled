9af2bf7cc255320f28bf593ab8441b1d4c9f1627
Henri Remonen <henri@remonen.fi>
2023-06-04 10:53:43 +0300

    Create entities directory

---
+++ src/server/Entities/user_entity.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+from marshmallow import Schema, fields, validate
+
+class NewUserSchema(Schema):
+    username = fields.Str(required=True, validate=validate.Length(min=3))
+    password = fields.Str(required=True, validate=validate.Length(min=8))
+    passwordConfirm = fields.Str(required=True, validate=validate.Equal(password))
+    
+
