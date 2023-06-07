9549b4abb4851f2c2c2f6bb5d6b58ac3e2d1bdcc
Henri Remonen <henri@remonen.fi>
2023-06-07 16:21:17 +0300

    Update models

--- src/server/database/models.py
+++ src/server/database/models.py
@@ -1,63 +1,87 @@
 
-DROP_TABLES = """
-    DROP TABLE IF EXISTS Users CASCADE
-"""
-
 CREATE_USERS = """
     CREATE TABLE IF NOT EXISTS Users (
-        id SERIAL PRIMARY KEY,
+        id UUID PRIMARY KEY,
         username TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL,
         role VARCHAR(8) DEFAULT 'user',
-        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0)
+        
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        deleted_at TIMESTAMP DEFAULT null
     )
 """
 
-CREATE_DISTILLERIES="""
-    CREATE TABLE IF NOT EXISTS Distilleries (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(50) NOT NULL,
-        location POINT NOT NULL,
-        country VARCHAR(50) NOT NULL,
-        year_established INT,
-        website VARCHAR(100),
-        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0)
-    )
-"""
-
-CREATE_WHISKEYS="""
-    CREATE TABLE IF NOT EXISTS Whiskeys (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(50) NOT NULL,
-        distillery_id INT NOT NULL,
-        type VARCHAR(50) NOT NULL,
-        age INT NOT NULL CHECK (age >= 0),
-        description TEXT,
-        FOREIGN KEY (distillery_id) REFERENCES Distilleries(id),
-        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0)
+CREATE_ENTITIES = """
+    CREATE TABLE IF NOT EXISTS Entities (
+        id UUID PRIMARY KEY,
+        
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        deleted_at TIMESTAMP DEFAULT null
     )
 """
 
 CREATE_RATINGS="""
     CREATE TABLE IF NOT EXISTS Ratings (
-        id SERIAL PRIMARY KEY,
-        user_id INT NOT NULL,
-        whiskey_id INT NOT NULL,
+        id UUID PRIMARY KEY,
+        user_id UUID NOT NULL,
+        entity_id UUID NOT NULL ,
         rating INT NOT NULL CHECK (rating >= 0 AND rating <= 5),
+        
         FOREIGN KEY (user_id) REFERENCES Users(id),
-        FOREIGN KEY (whiskey_id) REFERENCES Whiskeys(id),
-        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0)
+        FOREIGN KEY (entity_id) REFERENCES Entities(id),
+        
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        deleted_at TIMESTAMP DEFAULT null
     )
 """
 
 CREATE_COMMENTS="""
     CREATE TABLE IF NOT EXISTS Comments (
-        id SERIAL PRIMARY KEY,
-        user_id INT NOT NULL,
-        whiskey_id INT NOT NULL,
+        id UUID PRIMARY KEY,
+        user_id UUID NOT NULL,
+        entity_id UUID NOT NULL ,
         comment TEXT NOT NULL,
+        
         FOREIGN KEY (user_id) REFERENCES Users(id),
-        FOREIGN KEY (whiskey_id) REFERENCES Whiskeys(id),
-        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0)
+        FOREIGN KEY (entity_id) REFERENCES Entities(id),
+        
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        deleted_at TIMESTAMP DEFAULT null
+    )
+"""
+
+CREATE_DISTILLERIES="""
+    CREATE TABLE IF NOT EXISTS Distilleries (
+        id UUID PRIMARY KEY REFERENCES Entities(id),
+        name VARCHAR(50) NOT NULL,
+        location POINT NOT NULL,
+        country VARCHAR(5) NOT NULL,
+        year_established INT,
+        website VARCHAR(100),
+        
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        deleted_at TIMESTAMP DEFAULT null
+    )
+"""
+
+CREATE_WHISKEYS="""
+    CREATE TABLE IF NOT EXISTS Whiskeys (
+        id UUID PRIMARY KEY REFERENCES Entities,
+        name VARCHAR(50) NOT NULL,
+        distillery_id UUID NOT NULL,
+        type VARCHAR(50) NOT NULL,
+        age INT NOT NULL CHECK (age >= 0),
+        description TEXT,
+        
+        FOREIGN KEY (distillery_id) REFERENCES Distilleries(id),
+
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+        deleted_at TIMESTAMP DEFAULT null
     )
 """
--- src/server/db.py
+++ src/server/db.py
@@ -12,6 +12,7 @@ db = SQLAlchemy(app)
 
 with app.app_context():
     db.session.execute(text(models.CREATE_USERS))
+    db.session.execute(text(models.CREATE_ENTITIES))
     db.session.execute(text(models.CREATE_DISTILLERIES))
     db.session.execute(text(models.CREATE_WHISKEYS))
     db.session.execute(text(models.CREATE_RATINGS))
