85cb4e8b4340c9a7dc3b13739ab326dea56867c7
Henri Remonen <henri@remonen.fi>
2023-06-09 08:46:51 +0300

    Create validators for whiskeys

---
+++ src/server/Entities/whiskey_entity.py
@@ -0,0 +1,13 @@
+import datetime
+
+from marshmallow import Schema, fields, validate
+
+class NewWhiskeySchema(Schema):
+    name = fields.String(required=True)
+    distillery_id = fields.UUID(required=True)
+    type = fields.String(required=True)
+    age = fields.Integer(required=True, validate=validate.Range(min=0, max=datetime.datetime.now().year))
+    description = fields.String(required=True)
+    
+class UpdatedWhiskeySchema(Schema):
+    description = fields.String(required=True)
