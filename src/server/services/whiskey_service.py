ee5ce9e6bbc29e916da22390169ffff60d9ed114
Henri Remonen <henri@remonen.fi>
2023-06-09 08:49:01 +0300

    Create whiskey service methods

--- src/server/services/whiskey_service.py
+++ src/server/services/whiskey_service.py
@@ -1,14 +1,15 @@
 from sqlalchemy.engine.result import Row
-from ast import literal_eval
 
 from werkzeug.exceptions import Unauthorized
 
-from repositories.distillery_repository import distillery_repository
+from entities.whiskey_entity import NewWhiskeySchema
+from entities.whiskey_entity import UpdatedWhiskeySchema
+from repositories.whiskey_repository import whiskey_repository
 from services.user_service import user_service
 
-class DistilleryService:
-    def __init__(self, distillery_repository=distillery_repository) -> None:
-        self._distillery_repository = distillery_repository
+class WhiskeyService:
+    def __init__(self, whiskey_repository=whiskey_repository) -> None:
+        self._whiskey_repository = whiskey_repository
         
     def _to_json(self, query_result: Row) -> dict:
         """Generate JSON format entity for the query result
@@ -36,3 +37,66 @@ class DistilleryService:
             json_object["distillery_country"] = query_result.distillery_country
             
         return json_object
+    
+    def get_whiskey(self, id: str) -> Row:
+        query_result = self._whiskey_repository.get_whiskey(id)
+        
+        if not query_result: return None
+        
+        found_whiskey = self._to_json(query_result)
+        
+        return found_whiskey
+    
+    def get_whiskeys_by_country_iso(self, country_iso: str):        
+        query_result = self._whiskey_repository.get_whiskeys_by_country_iso(country_iso)
+        
+        if not query_result: return None
+        
+        found_whiskey = self._to_json(query_result)
+        
+        return found_whiskey
+    
+    def get_whiskeys(self):
+        query_result = self._whiskey_repository.get_whiskeys()
+        
+        if not query_result: return None
+        
+        found_whiskeys = map(self._to_json, query_result)
+        
+        return list(found_whiskeys)
+    
+    def create_whiskey(self, whiskey: dict) -> Row:
+        if not user_service.is_admin(): 
+            raise Unauthorized("You don't have permission to create new whiskeys")
+        
+        NewWhiskeySchema().load(whiskey)
+        
+        query_result = self._whiskey_repository.create_whiskey(whiskey)
+        
+        new_whiskey = self._to_json(query_result)
+        
+        return new_whiskey
+    
+    def update_whiskey_description(self, id: str, updates: dict) -> Row:
+        if not user_service.is_admin():
+            raise Unauthorized("You don't have permission to update whiskeys")
+        
+        UpdatedWhiskeySchema().load(updates)
+        
+        query_result = self._whiskey_repository.update_whiskey_description(id, updates)
+        
+        updated_whiskey = self._to_json(query_result)
+        
+        return updated_whiskey
+    
+    def delete_whiskey(self, id: str) -> Row:
+        if not user_service.is_admin():
+            raise Unauthorized("You don't have permission to delete whiskeys")
+        
+        query_result = self._whiskey_repository.delete_whiskey(id)
+        
+        deleted_whiskey = self._to_json(query_result)
+        
+        return deleted_whiskey
+    
+whiskey_service = WhiskeyService()
