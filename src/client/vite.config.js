795fc6dcec2e35deb52acf2c92ab8b9b23dbd3f4
Henri Remonen <henri@remonen.fi>
2023-06-10 15:02:23 +0300

    Init frontend with vite and tailwind css

---
+++ src/client/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://web:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
+})
