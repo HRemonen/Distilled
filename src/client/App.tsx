719a7590678a05a2887115be60f14dda60adb6be
Henri Remonen <henri@remonen.fi>
2023-06-11 12:50:09 +0300

    Extract map to own component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,43 +1,10 @@
-import React, { useState } from 'react'
-import Map, {
-  Marker,
-  Popup,
-  NavigationControl,
-  FullscreenControl,
-  ScaleControl,
-  GeolocateControl,
-} from 'react-map-gl'
-import { useDistilleries } from './services/distilleryService'
+import React from 'react'
+import RenderMap from './components/Map/RenderMap'
 
-const App = () => {
-  const { distilleryData, isLoading } = useDistilleries()
-  const [popUp, setPopUp] = useState(null)
-
-  if (isLoading) return null
-
-  console.log(distilleryData)
-
-  return (
-    <div>
-      <Map
-        initialViewState={{
-          latitude: 40,
-          longitude: -100,
-          zoom: 3.5,
-          bearing: 0,
-          pitch: 0,
-        }}
-        style={{ width: '100vw', height: '100vh' }}
-        mapStyle='mapbox://styles/mapbox/dark-v9'
-        mapboxAccessToken={import.meta.env.VITE_MAPBOX_KEY}
-      >
-        <GeolocateControl position='top-left' />
-        <FullscreenControl position='top-left' />
-        <NavigationControl position='top-left' />
-        <ScaleControl />
-      </Map>
-    </div>
-  )
-}
+const App = () => (
+  <section>
+    <RenderMap />
+  </section>
+)
 
 export default App
---
+++ src/client/components/Map/RenderMap.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import Map, {
+  Marker,
+  Popup,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl,
+} from 'react-map-gl'
+import { useDistilleries } from '../../services/distilleryService'
+
+const RenderMap = () => {
+  const { distilleryData, isLoading } = useDistilleries()
+  const [popUp, setPopUp] = useState(null)
+
+  if (isLoading) return null
+
+  console.log(distilleryData)
+
+  return (
+    <Map
+      initialViewState={{
+        latitude: 40,
+        longitude: -100,
+        zoom: 3.5,
+        bearing: 0,
+        pitch: 0,
+      }}
+      style={{ width: '100vw', height: '100vh' }}
+      mapStyle='mapbox://styles/mapbox/dark-v9'
+      mapboxAccessToken={import.meta.env.VITE_MAPBOX_KEY}
+    >
+      <GeolocateControl position='top-left' />
+      <FullscreenControl position='top-left' />
+      <NavigationControl position='top-left' />
+      <ScaleControl />
+    </Map>
+  )
+}
+
+export default RenderMap
