2ffafc0b2876391329534382dd44af8cf9f180c2
Henri Remonen <henri@remonen.fi>
2023-06-11 12:00:29 +0300

    Create query and api clients

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { QueryClientProvider } from 'react-query'
+import queryClient from './util/queryClient'
+
 import App from './App'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
 )
