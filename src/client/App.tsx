860ba69dbc405b9cceab104b8a46e5d81517a029
Henri Remonen <henri@remonen.fi>
2023-06-11 11:52:05 +0300

    Update env stuff

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Map, {
+  Marker,
+  Popup,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl,
+} from 'react-map-gl'
 
-function App() {
+const App = () => {
+  const [popUp, setPopUp] = useState(null)
+
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div>
+      <Map
+        initialViewState={{
+          latitude: 40,
+          longitude: -100,
+          zoom: 3.5,
+          bearing: 0,
+          pitch: 0,
+        }}
+        style={{ width: '100vw', height: '100vh' }}
+        mapStyle='mapbox://styles/mapbox/dark-v9'
+        mapboxAccessToken={import.meta.env.VITE_MAPBOX_KEY}
+      >
+        <GeolocateControl position='top-left' />
+        <FullscreenControl position='top-left' />
+        <NavigationControl position='top-left' />
+        <ScaleControl />
+      </Map>
+    </div>
   )
 }
 
