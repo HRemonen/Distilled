10ee05590496a317baefc3acd2f08bfcf4b69922
Henri Remonen <henri@remonen.fi>
2023-06-18 19:44:14 +0300

    Add a function to watch for the token expiration

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
+import jwt_decode from 'jwt-decode'
+
+import { AuthContext } from './contexts/AuthContext'
 
 import SearchBar from './components/map/SearchBar'
 import RenderMap from './components/map/RenderMap'
 import Logout from './components/authentication/Logout'
 
-const App = () => (
-  <section>
-    <Outlet />
-    <SearchBar />
-    <RenderMap />
-    <Logout />
-  </section>
-)
+const App = () => {
+  const { logout } = useContext(AuthContext)
+
+  const userToken = sessionStorage.getItem('token')
+  if (userToken) {
+    const { exp }: { exp: number } = jwt_decode(userToken)
+
+    if (exp * 1000 < Date.now()) {
+      console.log('TOKEN EXPIRED')
+      logout()
+    }
+  }
+  return (
+    <section>
+      <Outlet />
+      <SearchBar />
+      <RenderMap />
+      <Logout />
+    </section>
+  )
+}
 
 export default App
