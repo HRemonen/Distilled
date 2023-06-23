c589d1f689f2fbd9bfba73419db7df81828b7730
Henri Remonen <henri@remonen.fi>
2023-06-23 19:35:29 +0300

    Update eslintrc

--- src/client/components/admin/Admin.tsx
+++ src/client/components/admin/Admin.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import jwt_decode from 'jwt-decode'
 
