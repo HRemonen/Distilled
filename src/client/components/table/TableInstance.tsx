58544434354cfeb7ce9967c42f470b403ba79438
Henri Remonen <henri@remonen.fi>
2023-06-23 20:07:26 +0300

    Fix pagination for the distillery whiskeys

--- src/client/components/table/TableInstance.tsx
+++ src/client/components/table/TableInstance.tsx
@@ -24,44 +24,51 @@ const TableInstance = <T extends object>({
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 5,
+      },
+    },
   })
 
   if (!data) return null
 
   return (
-    <div className='relative ml-2 mt-4 flex max-w-full flex-col flex-wrap overflow-auto rounded-lg shadow-md'>
-      <table className='table-auto text-sm text-gray-400'>
-        <thead className='bg-gray-700 text-center text-xs uppercase text-gray-400'>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id} className='px-2 py-3'>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id} className='border-b border-gray-700 bg-gray-800'>
-              {row.getVisibleCells().map((cell) => (
-                <td
-                  className='whitespace-normal break-words px-2 py-4 text-left text-sm font-light'
-                  key={cell.id}
-                >
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className='relative ml-2 mt-4 flex max-w-full flex-col flex-wrap '>
+      <div className='max-h-[70vh] overflow-y-scroll rounded-lg scrollbar-hide'>
+        <table className='table-auto text-sm text-gray-400'>
+          <thead className='bg-gray-700 text-center text-xs uppercase text-gray-400'>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id} className='px-2 py-3'>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id} className='border-b border-gray-700 bg-gray-800'>
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    className='whitespace-normal break-words px-2 py-4 text-left text-sm font-light'
+                    key={cell.id}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <Pagination table={table} />
     </div>
   )
--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 export default {
   content: ['!./node_modules/**', './index.html', './**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.scrollbar-hide': {
+          /* IE and Edge */
+          '-ms-overflow-style': 'none',
+
+          /* Firefox */
+          'scrollbar-width': 'none',
+
+          /* Safari and Chrome */
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+        },
+      })
+    }),
+  ],
 }
