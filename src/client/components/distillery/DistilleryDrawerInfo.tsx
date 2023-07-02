204af63762eac138c202d1d5ed5da010a10b4965
Henri Remonen <henri@remonen.fi>
2023-07-02 13:56:39 +0300

    Update the coloring of the svg's

--- src/client/components/distillery/DistilleryDrawerInfo.tsx
+++ src/client/components/distillery/DistilleryDrawerInfo.tsx
@@ -18,7 +18,7 @@ const DistilleryDrawerInfo = () => {
       </Typography>
       <div className='mt-4 grid grid-cols-10 gap-x-2 pl-2'>
         <svg
-          className='col-span-1'
+          className='col-span-1 fill-gray-400'
           viewBox='0 0 16 16'
           version='1.1'
           width='16'
@@ -32,25 +32,27 @@ const DistilleryDrawerInfo = () => {
           {distilleryInfo.data.country}
         </Typography>
 
-        <svg
-          className='col-span-1'
-          aria-hidden='true'
-          height='16'
-          viewBox='0 0 16 16'
-          version='1.1'
-          width='16'
-          data-view-component='true'
-        >
-          <path d='m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z' />
-        </svg>
         {distilleryInfo.data.website ? (
-          <Typography
-            variant='link'
-            href={distilleryInfo.data.website}
-            className='col-span-9'
-          >
-            {distilleryInfo.data.website}
-          </Typography>
+          <>
+            <svg
+              className='col-span-1 fill-gray-400'
+              aria-hidden='true'
+              height='16'
+              viewBox='0 0 16 16'
+              version='1.1'
+              width='16'
+              data-view-component='true'
+            >
+              <path d='m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z' />
+            </svg>
+            <Typography
+              variant='link'
+              href={distilleryInfo.data.website}
+              className='col-span-9'
+            >
+              {distilleryInfo.data.website}
+            </Typography>
+          </>
         ) : null}
       </div>
     </div>
