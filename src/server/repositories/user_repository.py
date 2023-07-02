153f7a65cacad36a1951019687e1becc43622747
Henri Remonen <henri@remonen.fi>
2023-07-02 13:20:54 +0300

    Formatting of queries

--- src/server/repositories/user_repository.py
+++ src/server/repositories/user_repository.py
@@ -4,10 +4,11 @@ from werkzeug.security import generate_password_hash
 
 from db import db
 
+
 class UserRepository:
     def __init__(self, db=db):
         self._db = db
-        
+
     def get_user_role(self, username: str) -> Row:
         """Fetch the user's role from database using the username as query key.
 
@@ -16,17 +17,20 @@ class UserRepository:
 
         Returns:
             Row: Returning the found user role.
-        """        
-        user_input = {
-            "username": username
-        }
-        
-        sql = """
-            SELECT role FROM users WHERE username = :username
         """
-        
+        user_input = {"username": username}
+
+        sql = """
+            SELECT 
+                role 
+            FROM 
+                users 
+            WHERE 
+                username = :username
+        """
+
         return self._db.session.execute(text(sql), user_input).fetchone()
-    
+
     def get_user_id(self, username: str) -> Row:
         """Fethes an user id from database using the username as query key.
 
@@ -35,14 +39,17 @@ class UserRepository:
 
         Returns:
             Row: Returning the found user id.
-        """        
-        user_input = {
-            "username": username
-        }
-        sql = """
-            SELECT id FROM users WHERE username = :username
         """
-        
+        user_input = {"username": username}
+        sql = """
+            SELECT 
+                id 
+            FROM 
+                users 
+            WHERE
+                username = :username
+        """
+
         return self._db.session.execute(text(sql), user_input).fetchone()
 
     def register(self, user: object) -> Row:
@@ -50,15 +57,15 @@ class UserRepository:
 
         Args:
             user (object): User fields.
-        """        
+        """
         password_hash = generate_password_hash(user["password"])
-        
+
         register_input = {
             "username": user["username"],
             "password": password_hash,
-            "role": user.get("role", "user")
+            "role": user.get("role", "user"),
         }
-        
+
         sql = """
             INSERT INTO users (
                 username,
@@ -72,12 +79,12 @@ class UserRepository:
             )
             RETURNING *
         """
-        
+
         result = self._db.session.execute(text(sql), register_input)
         self._db.session.commit()
-        
+
         return result.fetchone()
-        
+
     def login(self, user: object) -> Row:
         """Fetches user information from the database on login.
 
@@ -86,16 +93,23 @@ class UserRepository:
 
         Returns:
             Row: Returning the found user.
-        """        
+        """
         login_input = {
             "username": user["username"],
         }
         sql = """
-            SELECT * 
-            FROM users 
-            WHERE username = :username
+            SELECT 
+                id,
+                username,
+                password,
+                role
+            FROM 
+                users
+            WHERE 
+                username = :username
         """
-        
-        return self._db.session.execute(text(sql), login_input).fetchone() 
+
+        return self._db.session.execute(text(sql), login_input).fetchone()
+
 
 user_repository = UserRepository()
--- src/server/services/user_service.py
+++ src/server/services/user_service.py
@@ -10,10 +10,11 @@ from flask_jwt_extended import get_jwt_identity
 from validators.user_validators import NewUserSchema
 from repositories.user_repository import user_repository
 
+
 class UserService:
     def __init__(self, user_repository=user_repository):
         self._user_repository = user_repository
-        
+
     def _to_json(self, query_result: Row) -> Dict:
         """Generate JSON format entity for the query result
         that is type of Row from the database
@@ -27,60 +28,60 @@ class UserService:
         json_object = {
             "id": query_result.id,
             "username": query_result.username,
-            "role": query_result.role
+            "role": query_result.role,
         }
-            
+
         return json_object
-        
+
     def is_admin(self) -> bool:
         """Service for checking if the authenticated user is admin.
-        
+
         Uses the JWT session to fetch the authenticated user from the authorization headers.
 
         Returns:
             bool: is the user admin or not.
-        """        
+        """
         username = get_jwt_identity()
-        
+
         user_role = self._user_repository.get_user_role(username)
-        
+
         return user_role.role == "admin"
-    
+
     def get_user_id(self) -> str:
         """Service for getting the authenticated user id.
-        
+
         Uses the JWT session to fetch the authenticated user from the authorization headers.
 
         Returns:
             str: user id.
-        """        
+        """
         username = get_jwt_identity()
-        
+
         user_id = self._user_repository.get_user_id(username)
-        
+
         return user_id.id
-    
+
     def register(self, user: Dict) -> Dict:
         """Service for registering a new user.
 
         Validates the user input.
-        
+
         Args:
             user (Dict): User model fields.
 
         Returns:
             Dict: Returning the newly created user object.
-        """        
+        """
         NewUserSchema().load(user)
         query_result = self._user_repository.register(user)
-        
+
         new_user = self._to_json(query_result)
-        
+
         return new_user
 
     def login(self, user: Dict) -> Dict:
         """Service for logging in a user.
-        
+
         Creates a JWT session token for the user that is returned.
 
         Args:
@@ -91,22 +92,20 @@ class UserService:
 
         Returns:
             Dict: JSON of the user and the JWT token.
-        """        
+        """
         query_result = self._user_repository.login(user)
-        
+
         found_user = self._to_json(query_result)
-        
-        if not found_user: return None
-        
+
+        if not found_user:
+            return None
+
         if not check_password_hash(query_result.password, user["password"]):
             raise Unauthorized("Invalid credentials")
-        
-        access_token = create_access_token(identity=found_user["username"])
 
-        return {
-            "user": found_user,
-            "token": access_token
-        }
+        access_token = create_access_token(identity=query_result.username)
 
-            
+        return {"user": found_user, "token": access_token}
+
+
 user_service = UserService()
